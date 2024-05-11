51f77765d8abd9553c1390194a3ee05d2a19086c
Sibusiso <144064884+SNMaduna@users.noreply.github.com>
2024-05-11 14:36:46 +0200

    Added things in the controller

---
+++ EduProfileAPI/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduProfileAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        //control hehe
+        ManualResetEvent _resetEvent = new ManualResetEvent(false); 
+        private readonly IUserService _userService;
+    }
+}
