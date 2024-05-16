fa5d97a5a0e77c95f20830500e78a600919e2fbc
Sibusiso <144064884+SNMaduna@users.noreply.github.com>
2024-05-16 13:15:17 +0200

    I have added the repository and endpoint with the simple get all. Its not working as of yet.
    also change your connection string befor trying to run.
    
    Added as well is the some other need items like the Irepo

---
+++ EduProfileAPI/DataAccessLayer/EduProfileDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using EduProfileAPI.Models.User;
+using EduProfileAPI.Models.Class;
+
+namespace EduProfileAPI.DataAccessLayer
+{
+    public class EduProfileDbContext : DbContext
+    {
+        public EduProfileDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public  DbSet<Class> Classes { get; set; }
+        
+    }
+}
