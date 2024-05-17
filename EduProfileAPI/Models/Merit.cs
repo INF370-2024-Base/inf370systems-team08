8786cf333a7dc1369e279605dcdbbd78198c4717
Kasmir Pillay <u22665006@up.ac.za>
2024-05-17 15:32:20 +0200

    i did all the merit things

---
+++ EduProfileAPI/Models/Merit.cs
@@ -0,0 +1,17 @@
+﻿namespace EduProfileAPI.Models
+{
+    public class Merit
+    {
+        [Key]
+        public Guid MeritId { get; set; }
+        public Guid MeritTypeId { get; set; }
+        public Guid EmployeeId { get; set; }
+
+        //[Required]
+        public string MeritName { get; set; }
+        public string MeritDescription { get; set; }
+
+
+
+    }
+}
---
+++ EduProfileAPI/Repositories/Implementation/MeritRepository.cs
@@ -0,0 +1,42 @@
+﻿using EduProfileAPI.DataAccessLayer;
+using EduProfileAPI.Models;
+using EduProfileAPI.Repositories.Interfaces;
+
+namespace EduProfileAPI.Repositories.Implementation
+{
+    public class MeritRepository:IMeritRepository
+    {
+        private readonly EduProfileDbContext _context;
+        public MeritRepository(EduProfileDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Merit[]> GetAllMeritsAsync()
+        {
+            IQueryable<Merit> query = _context.Merit;
+            return await query.ToArrayAsync();
+        }
+
+        public async Task<Merit> GetMeritAsync(int meritId)
+        {
+            IQueryable<Merit> query = _context.Merit.Where(c => c.MeritId == meritId);
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+    }
+}
