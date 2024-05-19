45b94a3e3ec4c25e9040f6438c0b70fdb00940ec
Tiffany Matthew <u22508776@up.ac.za>
2024-05-19 13:18:48 +0200

    Version 2 lol

---
+++ EduProfileApI/Repositories/Implementation/EmployeeRepository.cs
@@ -0,0 +1,56 @@
+﻿using EduProfileAPI.DataAccessLayer;
+using EduProfileAPI.Models;
+using EduProfileAPI.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace EduProfileAPI.Repositories.Implementation
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly EduProfileDbContext _context;
+        public EmployeeRepository(EduProfileDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee[]> GetAllEmployeesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Employee> GetEmployeeAsync(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task GetEmployeeAsync(Guid employeeId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Merit[]> IEmployeeRepository.GetAllEmployeesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Merit> IEmployeeRepository.GetEmployeeAsync(int employeeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EduProfileApI/Repositories/Interfaces/IEmployee.cs
@@ -0,0 +1,17 @@
+﻿using EduProfileAPI.Models;
+
+namespace EduProfileAPI.Repositories.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+
+        Task<Merit[]> GetAllEmployeesAsync();
+        Task<bool> SaveChangesAsync();
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+
+
+        Task<Merit> GetEmployeeAsync(int employeeId);
+        Task GetEmployeeAsync(Guid employeeId);
+    }
+}
