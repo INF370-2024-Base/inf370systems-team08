6797e19511b9029ba732d47fb286d13962d64938
TiffanyMatthew <u22508776@tuks.co.za>
2024-05-23 01:20:03 +0200

    Updated Employee CRUD

---
+++ EduProfileApI/Models/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace EduProfileAPI.Models
+{
+    public class Employee
+    {
+        [Key]
+        public Guid EmployeeId { get; set; }
+
+        public Guid EmployeeStatusId { get; set; }
+        [StringLength(30)]
+        public string FirstName { get; set; }
+
+        [StringLength(30)]
+        public string LastName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public bool Gender { get; set; }
+
+        [StringLength(10)]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(20)]
+        public string Address { get; set; }
+
+        [StringLength(20)]
+        public string Salary { get; set; }
+
+        [StringLength(13)]
+        public string IdentityNumber { get; set; }
+    }
+}
+
---
+++ EduProfileApI/Repositories/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,18 @@
+﻿using EduProfileAPI.Models;
+
+namespace EduProfileAPI.Repositories.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+
+        Task<Employee[]> GetAllEmployeesAsync();
+        Task<bool> SaveChangesAsync();
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+
+
+        Task<Employee> GetEmployeeAsync(int employeeId);
+        Task GetEmployeeAsync(Guid employeeId);
+    }
+}
+
