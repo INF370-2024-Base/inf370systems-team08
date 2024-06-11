0a68d0599e26b472cfe3f994cd3559d41c53c617
Sibusiso <144064884+SNMaduna@users.noreply.github.com>
2024-06-11 12:22:53 +0200

    Created the endpoint for the help doc. Need to finish the endpoint as well as understand the functionality we want to achieve.

---
+++ EduProfileAPI/Controllers/HelpDocController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EduProfileAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelpDocController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetHelpDoc")]
+    }
+}
