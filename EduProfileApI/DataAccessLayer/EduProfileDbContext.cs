7e1d15060d2a6bbcb24a7ab38f13debb2747f98b
Tiyad1606 <93083841+Tiyad1606@users.noreply.github.com>
2024-07-26 02:22:52 +0200

    fixes

--- EduProfileApI/DataAccessLayer/EduProfileDbContext.cs
+++ EduProfileApI/DataAccessLayer/EduProfileDbContext.cs
@@ -22,9 +22,7 @@ namespace EduProfileAPI.DataAccessLayer
         public DbSet<Student> Student { get; set; }
         public DbSet<StudentAnnouncement> StudentAnnouncement { get; set; }
         public DbSet<Employee> Employee { get; set; }
-
-
-
-
+        public DbSet<StudentIncident> studentIncident { get; set; }
+        public  DbSet<Parent> Parent { get; set; }
     }
 }
