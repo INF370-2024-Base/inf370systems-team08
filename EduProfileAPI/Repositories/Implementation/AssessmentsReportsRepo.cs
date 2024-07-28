6280066c4f523693dec4f7a69f4ae50cecae3068
Sibusiso <144064884+SNMaduna@users.noreply.github.com>
2024-07-28 17:03:04 +0200

    Reports done

--- EduProfileAPI/Repositories/Implementation/AssessmentsReportsRepo.cs
+++ EduProfileAPI/Repositories/Implementation/AssessmentsReportsRepo.cs
@@ -38,5 +38,44 @@ namespace EduProfileAPI.Repositories.Implementation
 
             return result;
         }
+
+        public async Task<List<AssessmentHighestMarkReportViewModel>> GetAssessmentHighestMarkReport()
+        {
+            var result = await _context.Assesment
+                .Join(
+                    _context.AssesmentMark,
+                    assessment => assessment.AssesmentId,
+                    mark => mark.AssesmentId,
+                    (assessment, mark) => new
+                    {
+                        Assessment = assessment,
+                        Mark = mark
+                    }
+                )
+                .Join(
+                    _context.Student,
+                    am => am.Mark.StudentId,
+                    student => student.StudentId,
+                    (am, student) => new
+                    {
+                        am.Assessment,
+                        am.Mark,
+                        Student = student
+                    }
+                )
+                .GroupBy(x => new { x.Assessment.SubjectId, x.Assessment.AssesmentId, x.Assessment.AssesmentName })
+                .Select(g => new AssessmentHighestMarkReportViewModel
+                {
+                    SubjectId = g.Key.SubjectId,
+                    AssesmentId = g.Key.AssesmentId,
+                    AssesmentName = g.Key.AssesmentName,
+                    HighestMark = g.Max(x => x.Mark.MarkAchieved),
+                    StudentFirstName = g.Where(x => x.Mark.MarkAchieved == g.Max(m => m.Mark.MarkAchieved)).Select(s => s.Student.FirstName).FirstOrDefault(),
+                    StudentLastName = g.Where(x => x.Mark.MarkAchieved == g.Max(m => m.Mark.MarkAchieved)).Select(s => s.Student.LastName).FirstOrDefault()
+                })
+                .ToListAsync();
+
+            return result;
+        }
     }
 }
---
+++ EduProfileAPI/ViewModels/AssessmentHighestMarkReportViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace EduProfileAPI.ViewModels
+{
+    public class AssessmentHighestMarkReportViewModel
+    {
+        public Guid SubjectId { get; set; }
+        public Guid AssesmentId { get; set; }
+        public string AssesmentName { get; set; }
+        public int HighestMark { get; set; }
+        public string StudentFirstName { get; set; }
+        public string StudentLastName { get; set; }
+    }
+}
