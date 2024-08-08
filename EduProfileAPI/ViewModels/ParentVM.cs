96ea3f3d73dc4fd654f1a89f2415a845a5f88aa1
Tiyad1606 <93083841+Tiyad1606@users.noreply.github.com>
2024-08-08 23:56:01 +0200

    fix parent add

---
+++ EduProfileAPI/ViewModels/ParentVM.cs
@@ -0,0 +1,22 @@
+﻿namespace EduProfileAPI.ViewModels
+{
+    public class ParentVM
+    {
+        public string Parent1Name { get; set; }
+        public bool Parent1MaritalStatus { get; set; }
+        public string Parent1Occupation { get; set; }
+        public string Parent1PhysicalAddress { get; set; }
+        public string Parent1PostalAddress { get; set; }
+        public string Parent1HomePhone { get; set; }
+        public string Parent1WorkPhone { get; set; }
+        public string Parent1CellPhone { get; set; }
+        public string Parent2Name { get; set; }
+        public bool Parent2MaritalStatus { get; set; }
+        public string Parent2Occupation { get; set; }
+        public string Parent2PhysicalAddress { get; set; }
+        public string Parent2PostalAddress { get; set; }
+        public string Parent2HomePhone { get; set; }
+        public string Parent2WorkPhone { get; set; }
+        public string Parent2CellPhone { get; set; }
+    }
+}
