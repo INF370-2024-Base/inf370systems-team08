72a6f9dfaab7c330e971bb96a0b2942cd79a0db5
Tiyad1606 <93083841+Tiyad1606@users.noreply.github.com>
2024-08-08 16:02:56 +0200

    View and Update Help Document

--- EduProfileAPI/Controllers/HelpDocController.cs
+++ EduProfileAPI/Controllers/HelpDocController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using EduProfileAPI.Models;
 
 namespace EduProfileAPI.Controllers
 {
@@ -7,6 +8,33 @@ namespace EduProfileAPI.Controllers
     [ApiController]
     public class HelpDocController : ControllerBase
     {
-        
+        // Hardcoded help document content
+        public static string helpDocument = "Welcome to the EduProfile Help Document." +
+                              "Here you will find all the information you need to navigate and use the system effectively. " +
+                              "This document is designed to assist users in understanding the various features and functionalities of our system." +
+                              "Getting Started:" +
+                              "- How to log in:";
+
+        // GET: api/helpdoc
+        [HttpGet]
+        [Route("GetHelpDoc")]
+        public ActionResult<string> GetHelpDocument()
+        {
+            return Ok(helpDocument);
+        }
+
+        // PUT: api/helpdoc
+        [HttpPut]
+        public IActionResult UpdateHelpDocument([FromBody] HelpDoc model)
+        {
+            if (model == null || string.IsNullOrEmpty(model.Content))
+            {
+                return BadRequest("The help document content cannot be empty.");
+            }
+
+            helpDocument = model.Content;
+            return Ok("Help document updated successfully.");
+        }
+
     }
 }
