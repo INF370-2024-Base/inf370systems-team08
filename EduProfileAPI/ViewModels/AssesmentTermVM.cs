fe7f06b5d736736386246b56a4fdb6050149456c
Tiyad1606 <93083841+Tiyad1606@users.noreply.github.com>
2024-09-26 20:55:32 +0200

    Term mode and view model

---
+++ EduProfileAPI/ViewModels/AssesmentTermVM.cs
@@ -0,0 +1,12 @@
+﻿using EduProfileAPI.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace EduProfileAPI.ViewModels
+{
+    public class AssesmentTermVM
+    {
+        public Guid AssesmentId { get; set; }
+        public int Term { get; set; }
+        public decimal Weighting { get; set; }
+    }
+}
