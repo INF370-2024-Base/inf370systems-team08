09964ed28dec57c35cc40d0da5ac59710b5b6b33
Sibusiso <144064884+SNMaduna@users.noreply.github.com>
2024-09-28 22:06:11 +0200

    dd

--- EduProfileAPI/DataAccessLayer/EduProfileDbContext.cs
+++ EduProfileAPI/DataAccessLayer/EduProfileDbContext.cs
@@ -58,7 +58,7 @@ namespace EduProfileAPI.DataAccessLayer
         public DbSet<StudentUser> StudentUser { get; set; }
         public DbSet<Permissions> Permissions { get; set; }
         public DbSet<UserPermissions> UserPermissions { get; set; }
-      //  public DbSet<BackupSchedule> BackupSchedule { get; set; }
+        //  public DbSet<BackupSchedule> BackupSchedule { get; set; }
         public DbSet<AuditTrail> AuditTrail { get; set; }
         public DbSet<AssesmentTerm> AssesmentTerm { get; set; }
 
